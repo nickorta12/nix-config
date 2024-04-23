88d7b8264101614db3bd5f2c34d5dae6a4a2dc4f
Nicholas Orta <nickorta12@gmail.com>
2024-04-22 23:16:16 -0600

    Reorg

--- home/default.nix
+++ home/default.nix
@@ -13,78 +13,6 @@
     ./zsh.nix
   ];
 
-  home.packages = with pkgs; [
-    alejandra
-    neofetch
-    fzf
-    jq
-    aria2
-    socat
-    nmap
-    ipcalc
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-
-    nix-output-monitor
-    glow
-    btop
-    iotop
-    iftop
-
-    strace
-    lsof
-    sysstat
-    ethtool
-    pciutils
-    usbutils
-    procs
-    hexyl
-    hyperfine
-    bottom
-    just
-    bandwhich
-    lazygit
-    tmux
-    zellij
-    tokei
-    xh
-
-    nil
-    nixpkgs-fmt
-  ];
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-  };
-
-  programs.atuin = {
-    enable = true;
-  };
-
-  programs.htop = {
-    enable = true;
-    settings = {
-      show_program_path = 0;
-    };
-  };
-
   programs.firefox = {
     enable = true;
     profiles = {
@@ -102,10 +30,6 @@
   programs.vscode = {
     enable = true;
     enableUpdateCheck = false;
-  };
-
-  programs.zoxide = {
-    enable = true;
   };
 
   home.stateVersion = "23.11";
--- home/shell.nix
+++ home/shell.nix
@@ -1,7 +1,79 @@
 {...}: {
+  # -- CLI Packages --
+
+  home.packages = with pkgs; [
+    alejandra
+    aria2
+    bandwhich
+    bottom
+    btop
+    ethtool
+    file
+    fzf
+    gawk
+    glow
+    gnupg
+    gnused
+    gnutar
+    hexyl
+    hyperfine
+    iftop
+    iotop
+    ipcalc
+    jq
+    just
+    lazygit
+    lsof
+    neofetch
+    nil
+    nix-output-monitor
+    nixpkgs-fmt
+    nmap
+    pciutils
+    procs
+    socat
+    strace
+    sysstat
+    tmux
+    tokei
+    tree
+    usbutils
+    which
+    xh
+    zellij
+    zstd
+  ];
+
+  # -- Modules Config --
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  programs.atuin = {
+    enable = true;
+  };
+
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
     git = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+  };
+
+  programs.htop = {
+    enable = true;
+    settings = {
+      show_program_path = 0;
+    };
   };
 }
