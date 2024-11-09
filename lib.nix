8989d1c6212439ab7bb6b6a89e4e0512e2b5c8ce
Nicholas Orta <nickorta12@gmail.com>
2024-11-08 22:34:22 -0700

    fix volta root

--- lib.nix
+++ lib.nix
@@ -26,25 +26,22 @@
     root ? false,
     system,
   }: {
-    home-manager =
-      {
-        backupFileExtension = "bak";
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users."${user}" = commonHome {inherit isLinux hostname;};
-        extraSpecialArgs = {
-          inherit self inputs isLinux outputs hostname desktop system;
-          isDarwin = !isLinux;
-        };
-      }
-      // lib.optionalAttrs root {
-        users.root = {...}: {
-          imports = [
-            inputs.nixvim.homeManagerModules.nixvim
-            ./nixos/${hostname}/root.nix
-          ];
-        };
+    home-manager = {
+      backupFileExtension = "bak";
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      users."${user}" = commonHome {inherit isLinux hostname;};
+      users.root = lib.optionalAttrs root ({...}: {
+        imports = [
+          inputs.nixvim.homeManagerModules.nixvim
+          ./nixos/${hostname}/root.nix
+        ];
+      });
+      extraSpecialArgs = {
+        inherit self inputs isLinux outputs hostname desktop system;
+        isDarwin = !isLinux;
       };
+    };
   };
   mkHomeNixos = {
     hostname,
