7308e3741d70c51141481a4dd18e3ef0bb5b3579
Nicholas Orta <nickorta12@gmail.com>
2024-11-16 19:10:19 -0700

    try dnsmasq

--- nixos/volta/network.nix
+++ nixos/volta/network.nix
@@ -18,6 +18,11 @@ in {
       allowedTCPPorts = [53 4000];
       allowedUDPPorts = [53 67];
     };
+    nat = {
+      enable = true;
+      internalInterfaces = ["ve-blocky"];
+      externalInterface = "eno4";
+    };
   };
 
   systemd.network = {
@@ -33,93 +38,127 @@ in {
       dns = dns;
     };
   };
-  services = {
-    tailscale.enable = true;
 
-    # DNS blocking
-    blocky = {
-      enable = true;
-      settings = {
-        upstreams.groups.default = dns;
-        bootstrapDns = [
-          {upstream = "8.8.8.8";}
-          {upstream = "8.8.4.4";}
-        ];
-        ports = {
-          dns = "${ip}:53";
-          http = 4000;
+  containers.blocky = {
+    privateNetwork = true;
+    autoStart = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
+    config = {lib, ...}: {
+      system.stateVersion = "24.05";
+      networking = {
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [53 4000];
+          allowedUDPPorts = [53];
         };
-        blocking = {
-          loading = {
-            refreshPeriod = "24h";
-            downloads.timeout = "30s";
-          };
-          denylists = {
-            StevenBlack = [
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts"
-            ];
-          };
-          allowlists = {
-            StevenBlack = [
-              "${./allow-list.txt}"
-            ];
-          };
-          clientGroupsBlock = {
-            default = [
-              "StevenBlack"
-            ];
-          };
-        };
-        queryLog = {
-          type = "csv-client";
-          target = "/var/log/blocky";
-          logRetentionDays = 5;
-        };
+        useHostResolvConf = lib.mkForce false;
       };
-    };
-
-    kea.dhcp4 = {
-      enable = true;
-      settings = {
-        interfaces-config = {
-          interfaces = ["eno4"];
+      systemd.services.blocky.serviceConfig = {
+        LogsDirectory = "blocky";
+      };
+      services.blocky = {
+        enable = true;
+        settings = {
+          upstreams.groups.default = dns;
+          bootstrapDns = [
+            {upstream = "8.8.8.8";}
+            {upstream = "8.8.4.4";}
+          ];
+          ports = {
+            dns = 53;
+            http = 4000;
+          };
+          blocking = {
+            loading = {
+              refreshPeriod = "24h";
+              downloads.timeout = "30s";
+            };
+            denylists = {
+              StevenBlack = [
+                "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts"
+              ];
+            };
+            allowlists = {
+              StevenBlack = [
+                "${./allow-list.txt}"
+              ];
+            };
+            clientGroupsBlock = {
+              default = [
+                "StevenBlack"
+              ];
+            };
+          };
+          queryLog = {
+            type = "csv-client";
+            target = "/var/log/blocky";
+            logRetentionDays = 5;
+          };
         };
-        lease-database = {
-          name = "/var/lib/kea/dhcp4.leases";
-          persist = true;
-          type = "memfile";
-          lfc-interval = 1000;
-          max-row-errors = 100;
-        };
-        rebind-timer = 2000;
-        renew-timer = 1000;
-        valid-lifetime = 4000;
-        option-data = [
-          {
-            name = "routers";
-            data = "10.25.0.1";
-          }
-          {
-            name = "domain-name-servers";
-            data = "${ip}";
-          }
-        ];
-        subnet4 = [
-          {
-            id = 1;
-            subnet = "10.25.0.0/24";
-            pools = [
-              {
-                pool = "10.25.0.10 - 10.25.0.200";
-              }
-            ];
-          }
-        ];
       };
     };
   };
 
-  systemd.services.blocky.serviceConfig = {
-    LogsDirectory = "blocky";
+  services = {
+    tailscale.enable = true;
+
+    # kea.dhcp4 = {
+    #   enable = true;
+    #   settings = {
+    #     interfaces-config = {
+    #       interfaces = ["eno4"];
+    #     };
+    #     lease-database = {
+    #       name = "/var/lib/kea/dhcp4.leases";
+    #       persist = true;
+    #       type = "memfile";
+    #       lfc-interval = 1000;
+    #       max-row-errors = 100;
+    #     };
+    #     rebind-timer = 2000;
+    #     renew-timer = 1000;
+    #     valid-lifetime = 4000;
+    #     option-data = [
+    #       {
+    #         name = "routers";
+    #         data = "10.25.0.1";
+    #       }
+    #       {
+    #         name = "domain-name-servers";
+    #         data = "${ip}";
+    #       }
+    #     ];
+    #     subnet4 = [
+    #       {
+    #         id = 1;
+    #         subnet = "10.25.0.0/24";
+    #         pools = [
+    #           {
+    #             pool = "10.25.0.10 - 10.25.0.200";
+    #           }
+    #         ];
+    #       }
+    #     ];
+    #   };
+    # };
+
+    dnsmasq = {
+      enable = true;
+      resolveLocalQueries = false;
+      settings = {
+        listen-address = [
+          "127.0.0.1"
+          "10.25.0.2"
+        ];
+        no-resolv = true;
+        server = ["192.168.100.11"];
+        dhcp-range = "10.25.0.10,10.25.0.200,12h";
+        dhcp-option = "3,10.25.0.1";
+        local-service = true;
+        bogus-priv = true;
+        domain-needed = true;
+      };
+    };
   };
 }
