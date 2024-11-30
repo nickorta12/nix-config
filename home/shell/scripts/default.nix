c98b28e1af9701e9674ab0b6f7cec8308d425253
Nicholas Orta <nickorta12@gmail.com>
2024-11-30 09:18:07 -0700

    extra paths

--- home/shell/scripts/default.nix
+++ home/shell/scripts/default.nix
@@ -7,10 +7,18 @@
     (pkgs.writeShellScriptBin "nixpkgs-dir" ''
       echo $(nix eval --impure --raw --expr '(builtins.getFlake "${self}").inputs.nixpkgs.sourceInfo')
     '')
+    (pkgs.writeShellScriptBin "nix-darwin-dir" ''
+      echo $(nix eval --impure --raw --expr '(builtins.getFlake "${self}").inputs.nix-darwin.sourceInfo')
+    '')
+    (pkgs.writeShellScriptBin "home-manager-dir" ''
+      echo $(nix eval --impure --raw --expr '(builtins.getFlake "${self}").inputs.home-manager.sourceInfo')
+    '')
   ];
 
   home.shellAliases = {
     cd-nixpkgs = "cd $(nixpkgs-dir)";
+    cd-nix-darwin = "cd $(nix-darwin-dir)";
+    cd-home-manager = "cd $(home-manager-dir)";
     nix-repl = "nix repl --expr '{pkgs = import <nixpkgs>{};}'";
     flake-repl = "nix repl --expr 'builtins.getFlake \"${self}\"'";
   };
