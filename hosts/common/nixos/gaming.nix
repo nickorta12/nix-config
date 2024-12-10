bf5e0f30c4cc99b48397f55d7e75472f7285a75a
Nicholas Orta <nickorta12@gmail.com>
2024-12-09 21:29:26 -0700

    Minecraft

--- hosts/common/nixos/gaming.nix
+++ hosts/common/nixos/gaming.nix
@@ -1,10 +1,12 @@
-{
+{pkgs, ...}: {
   # Gaming settings
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+  environment.systemPackages = [pkgs.prismlauncher];
+
   programs.gamemode.enable = true;
   hardware.graphics.enable = true;
 }
