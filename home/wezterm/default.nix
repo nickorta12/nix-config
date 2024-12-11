ab6347ff1569115af087320c17f803a23b485148
Nicholas Orta <nickorta12@gmail.com>
2024-12-10 21:04:30 -0700

    Make zsh on mac faster
    For whatever reason wezterm integration is slow on macos while fine on
    Linux

--- home/wezterm/default.nix
+++ home/wezterm/default.nix
@@ -1,9 +1,10 @@
-{pkgs, ...}: {
+{isLinux, ...}: {
   programs = {
     # zsh.initExtra = ''source "${pkgs.wezterm}/etc/profile.d/wezterm.sh"'';
     wezterm = {
       enable = true;
       extraConfig = builtins.readFile ./wezterm.lua;
+      enableZshIntegration = isLinux;
     };
   };
 }
