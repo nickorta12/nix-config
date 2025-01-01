338169158ef47646e9195be11912ee2bf1bc9430
Nicholas Orta <nickorta12@gmail.com>
2024-12-31 21:18:58 -0700

    Ghostty ctrl-enter tweaks

--- home/ghostty/default.nix
+++ home/ghostty/default.nix
@@ -9,4 +9,8 @@
     ]
     ++ lib.optionals isLinux [(lib.readFile ./ghostty-linux.cfg)]
     ++ lib.optionals isDarwin [(lib.readFile ./ghostty-mac.cfg)]);
+
+  home.shellAliases = {
+    ghostty-docs = "ghostty +show-config --default --docs | bat -lini --file-name ghostty-docs";
+  };
 }
